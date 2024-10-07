dd183de7825c7f68e495d6c8141ba099092cfca1
joes <joeswaruwu@gmail.com>
2024-10-07 10:45:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uts/MainActivity2.kt
@@ -0,0 +1,26 @@
+package com.example.uts
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.uts.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        with(binding) {
+            home.setOnClickListener {
+                val intentToMainActivity = Intent(this@MainActivity2, MainActivity::class.java).apply {
+
+                }
+                startActivity(intentToMainActivity)
+            }
+        }
+    }
+}
